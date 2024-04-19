4391f0d90c93acc358041c993650212efddc5bd3
Kehad <keahnney01@gmail.com>
2024-04-19 18:29:12 +0100

    3  commit

--- src/components/skills/skillsList.jsx
+++ src/components/skills/skillsList.jsx
@@ -3,11 +3,11 @@
 
 const SkillsList = function (props) {
   return (
-    <div className="flex justify-between align-center rounded-full px-8 py-2  w-9/12">
-      <p className="font-judson text-2xl text-primary font-bold">
+    <div className="flex justify-between items-center rounded-full h-20  w-9/12 bg-sub">
+      <p className="font-judson text-3xl text-primary font-bold ml-6">
         {props.name}
       </p>
-      <div className="flex items-center justify-center bg-green-800 text-gray-100 border border-gray-700 border-l-4 rounded-l-lg py-2 px-8 mr-6">
+      <div className="flex items-center justify-center      mr-6">
         {props.link}
       </div>
     </div>
